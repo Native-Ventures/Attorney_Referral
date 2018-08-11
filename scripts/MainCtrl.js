f0e2d30480db4709193ed2399174dc678ab04a58
Gal Ben-Chanoch <gbenchanoch@gmail.com>
2018-08-11 22:03:27 +0300

    Update 11.08.2018

--- scripts/MainCtrl.js
+++ scripts/MainCtrl.js
@@ -243,7 +243,7 @@
           className: 'row',
           fieldGroup:[
             {
-              className: 'col-xs-6',
+              className: 'col-xs-4',
               key: 'referring_first',
               type: 'input',
               templateOptions: {
@@ -254,7 +254,7 @@
               }
             },
             {
-              className: 'col-xs-6',
+              className: 'col-xs-4',
               key: 'referring_last',
               type: 'input',
               templateOptions: {
@@ -264,42 +264,238 @@
                   required: true,
               }
             },
-          ]
-        },
-        {
-          className: 'row',
-          fieldGroup:[
             {
-              className: 'col-xs-6',
-              key: 'recommended_first',
+              className: 'col-xs-4',
+              key: 'referring_email',
               type: 'input',
               templateOptions: {
-                  type: 'text',
-                  label: 'Recommended Party\'s First Name',
-                  placeholder: 'Enter the recommended party\'s first name',
-                  required: true,
-              }
-            },
-            {
-              className: 'col-xs-6',
-              key: 'recommended_last',
-              type: 'input',
-              templateOptions: {
-                  type: 'text',
-                  label: 'Recommended Party\'s Last Name',
-                  placeholder: 'Enter the recommended party\'s last name',
+                  type: 'email',
+                  label: 'Referring Party\'s Email',
+                  placeholder: 'Enter your last name',
                   required: true,
               }
             },
           ]
         },
         {
-          type: 'textarea',
-          templateOptions: {
-            label: 'Recommendation Notes',
-            placeholder: 'Please enter any notes about why you would like to recommend this attorney',
-            rows: 5
-          },
+        type: 'repeatSection',
+        key: 'attorneys',
+        templateOptions: 
+          {
+            btnClass: "btn btn-block btn-attorney",
+            btnText: "Recommend Another Attorney",
+            removebtnText: "Remove Attorney",
+            fields: 
+              [
+                {
+                  className: 'row',
+                  fieldGroup: [
+                    {
+                      className: 'col-xs-3',
+                      type: 'input',
+                      key: 'firstName',
+                      templateOptions: {
+                        label: 'First Name',
+                        placeholder: 'Enter attorney\'s first name',
+                        required: true
+                      }
+                    },
+                    {
+                      className: 'col-xs-3',
+                      type: 'input',
+                      key: 'lastName',
+                      templateOptions: {
+                        label: 'Last Name',
+                        placeholder: 'Enter attorney\'s last name',
+                        required: true
+                      },
+                    },
+                    {
+                      className: 'col-xs-3',
+                      key: 'email',
+                      type: 'input',
+                      templateOptions: {
+                        type: 'email',
+                        label: 'Email',
+                        placeholder: 'Enter email',
+                        required: true
+                      }
+                    },
+                    {
+                      className: 'col-xs-3',
+                      type: "radioType",
+                      key: "performance",
+                      templateOptions: {
+                        inline: true,
+                        label: "Performance - 5 is perfect",
+                        // theme: "custom",
+                        labelProp: "score",
+                        valueProp: "id",
+                        options: [
+                            {score: "1", id: 1},
+                            {score: "2", id: 2},
+                            {score: "3", id: 3},
+                            {score: "4", id: 4},
+                            {score: "5", id: 5},
+                            {score: "NA", id: "NA"}
+                        ],
+                        required: true
+                      }
+                    }
+                  ]
+                },
+                {
+                  className: 'row',
+                  fieldGroup:[
+                    {
+                      className: 'col-xs-3',
+                      key: 'title',
+                      type: 'select',
+                      templateOptions: {
+                        label: 'Title',
+                        options: [
+                          {name: 'Partner', value: 'partner'},
+                          {name: 'Associate', value: 'associate'},
+                          {name: 'Counsel', value: 'counsel'},
+                        ],
+                        required: false
+                      }
+                    },
+                    {
+                      className: 'col-xs-3',
+                      key: 'practice_area',
+                      type: 'select',
+                      templateOptions: {
+                        label: 'Practice Area',
+                        options: [
+                          {name: 'M&A', value: 'mna'},
+                          {name: 'Finance', value: 'finance'},
+                          {name: 'Tax', value: 'tax'},
+                          {name: 'Intellectual Property', value: 'ip'},
+                          {name: 'Real Estate', value: 'realestate'},
+                          {name: 'Labor/Executive Compensation/Benefits', value: 'l_ec_b'},
+                          {name: 'Other', value: 'other'},
+                        ],
+                        required: false
+                      }
+                    },
+                    {
+            className: 'col-xs-3',
+            key: 'office',
+            type: 'select',
+            templateOptions: {
+              label: 'Office',
+              options: [
+                {name: 'New York City', value: 'nyc'},
+                {name: 'Chicago', value: 'chg'},
+                {name: 'Other', value: 'other'},
+              ],
+                required: false
+            },
+        },
+                    // {
+                    //   className: 'col-xs-3',
+                    //   key: 'other_role',
+                    //   type: 'input',
+                    //   templateOptions: {
+                    //       label: 'Other Role',
+                    //         required: false,
+                    //         disabled: true
+                    //   },
+
+                    //   expressionProperties: {
+                    //     'templateOptions.disabled': function($viewValue, $modelValue, scope) {
+                    //       return scope.model.role !== 'other';
+                    //     }
+                    //   },
+
+                    //   // hideExpression: function($viewValue, $modelValue, scope) {
+                    //   //         return scope.model.role !== 'other';
+                    //   //       },
+
+                    // },
+                    {
+                      className: 'col-xs-3',
+                      type: 'select',
+                      key: 'classyear',
+                      templateOptions: {
+                        label: 'Class Year',
+                        placeholder: 'Enter attorney\'s class year',
+                        options: [
+                          {name: '2018', value: '2018'},
+                          {name: '2017', value: '2017'},
+                          {name: '2016', value: '2016'},
+                          {name: '2015', value: '2015'},
+                          {name: '2014', value: '2014'},
+                          {name: '2013', value: '2013'},
+                          {name: '2012', value: '2012'},
+                          {name: '2011', value: '2011'},
+                          {name: '2010', value: '2010'},
+                          {name: '2009', value: '2009'},
+                          {name: '2008', value: '2008'},
+                          {name: '2007', value: '2007'},
+                          {name: '2006', value: '2006'},
+                          {name: '2005', value: '2005'},
+                          {name: '2004', value: '2004'},
+                          {name: '2003', value: '2003'},
+                          {name: '2002', value: '2002'},
+                          {name: '2001', value: '2001'},
+                          {name: '2000', value: '2000'},
+                          {name: '1999', value: '1999'},
+                          {name: '1998', value: '1998'},
+                          {name: '1997', value: '1997'},
+                          {name: '1996', value: '1996'},
+                          {name: '1995', value: '1995'},
+                          {name: '1994', value: '1994'},
+                          {name: '1993', value: '1993'},
+                          {name: '1992', value: '1992'},
+                          {name: '1991', value: '1991'},
+                          {name: '1990', value: '1990'},
+                        ],
+                        required: false
+                      }
+                    },
+                  ]
+                },
+                {
+                  className: 'row',
+                  fieldGroup: [
+                    {
+                      className: 'col-xs-6',
+                      type: 'input',
+                      key: 'firm',
+                      templateOptions: {
+                        label: 'Firm',
+                        placeholder: 'Enter attorney\'s firm',
+                        required: false
+                      },
+                    },
+                    {
+                      className: 'col-xs-6',
+                      type: 'input',
+                      key: 'firmlink',
+                      templateOptions: {
+                        label: 'Firm Link',
+                        placeholder: 'Enter attorney\'s firm link',
+                        required: false
+                      },
+                    }
+                  ]
+                },
+                {
+                  type: 'textarea',
+                  templateOptions: {
+                    label: 'Recommendation Notes',
+                    placeholder: 'Please enter any notes about why you would like to recommend this attorney',
+                    rows: 3
+                  },
+                },
+               
+                // {
+                //   template: '<hr style="border-top: dashed 1px; border-color:black;"/>'
+                // }
+              ]
+            }
         },
       ];
     }
